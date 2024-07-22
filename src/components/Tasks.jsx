55dad1dd150b55fbeb79bde58a84ac2e22721d09
Harshit Awasthi <awasthiharshit8@gmail.com>
2024-07-22 12:43:48 +0530

    "bug fixes and enhancements"

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,38 +1,30 @@
-import Tasks from './Tasks.jsx';
+import NewTask from './NewTask.jsx';
 
-export default function SelectedProject({
-  project,
-  onDelete,
-  onAddTask,
-  onDeleteTask,
-  tasks
-}) {
-  const formattedDate = new Date(project.dueDate).toLocaleDateString('en-US', {
-    year: 'numeric',
-    month: 'short',
-    day: 'numeric',
-  });
-
+export default function Tasks({ tasks, onAdd, onDelete }) {
   return (
-    <div className="w-[35rem] mt-16">
-      <header className="pb-4 mb-4 border-b-2 border-stone-300">
-        <div className="flex items-center justify-between">
-          <h1 className="text-3xl font-bold text-stone-600 mb-2">
-            {project.title}
-          </h1>
-          <button
-            className="text-stone-600 hover:text-stone-950"
-            onClick={onDelete}
-          >
-            Delete
-          </button>
-        </div>
-        <p className="mb-4 text-stone-400">{formattedDate}</p>
-        <p className="text-stone-600 whitespace-pre-wrap">
-          {project.description}
+    <section>
+      <h2 className="text-2xl font-bold text-stone-700 mb-4">Tasks</h2>
+      <NewTask onAdd={onAdd} />
+      {tasks.length === 0 && (
+        <p className="text-stone-800 my-4">
+          This project does not have any tasks yet.
         </p>
-      </header>
-      <Tasks onAdd={onAddTask} onDelete={onDeleteTask} tasks={tasks} />
-    </div>
+      )}
+      {tasks.length > 0 && (
+        <ul className="p-4 mt-8 rounded-md bg-stone-100">
+          {tasks.map((task) => (
+            <li key={task.id} className="flex justify-between my-4">
+              <span>{task.text}</span>
+              <button
+                className="text-stone-700 hover:text-red-500"
+                onClick={() => onDelete(task.id)}
+              >
+                Clear
+              </button>
+            </li>
+          ))}
+        </ul>
+      )}
+    </section>
   );
 }
